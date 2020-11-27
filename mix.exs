bb0960a4c397799155d8fa6caedf6b3cbf7dc45e
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-11-28 00:04:07 +0100

    Add dialyzer

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Advent.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:test, :dev], runtime: false}
+      {:credo, "~> 1.5", only: [:test, :dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:test, :dev], runtime: false}
     ]
   end
 end
