54b887cb8766d15989353c950e154e2b2dc2b176
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-11-27 23:56:59 +0100

    Mix new

---
+++ lib/advent2016.ex
@@ -0,0 +1,18 @@
+defmodule Advent2016 do
+  @moduledoc """
+  Documentation for `Advent2016`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advent2016.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
